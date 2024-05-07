6c7a2f39eb8d38161e32cf7a8bc86c7fc173759b
tamu309 <tamannaera309@gmail.com>
2024-05-07 22:26:35 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let input=document.getElementById('inputNumber');
+let input=document.querySelectorAll('button');
+let string="";
+let arr=array.from('buttons')
+array.forEach(button => {
+    button.addEventListener('click,(e)+>{
+        if (e.target.innerHTML == "=") {
+            string=eval('string');
+            input.value=string;
+        
+    }
+    else if(e.target.innerHTML == "AC"){
+        string="";
+        input.value=string;
+    }
+    ')
+    k
+});
